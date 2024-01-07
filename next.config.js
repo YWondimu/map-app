6d097a2577566744fcc00b0cc330b06fc238cb83
Yohannes Wondimu <yohannes.wondimu@gmail.com>
2024-01-07 03:34:43 -0400

    save work

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    //not really sure what these do
+    output: 'export',
+    distDir: 'dist',
+    images: {
+        unoptimized: true,
+    },
+}
 
-module.exports = nextConfig
+module.exports = nextConfig;
